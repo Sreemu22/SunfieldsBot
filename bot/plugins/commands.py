754fc00401e2f02180a940db24ce703ea84bb9b6
Sreemu22 <111636610+Sreemu22@users.noreply.github.com>
2022-08-20 16:06:37 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -28,14 +28,14 @@ async def start(bot, update):
             await update.reply_cached_media(
                 file_id,
                 quote=True,
-                caption = caption,
+                caption = "@Sunfields,
                 parse_mode="html",
                 reply_markup=InlineKeyboardMarkup(
                     [
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    'Lands', url="https://t.me/Sunfields"
                                 )
                         ]
                     ]
